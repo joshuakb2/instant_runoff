dc3d42f00ce190b71e22ad89244e05abf1c562a4
Joshua Baker <joshuakb2@gmail.com>
2026-02-23 01:03:35 -0700

    Add TypeScript

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 import * as fs from 'fs';
 import { markdownToPdf } from '@mdpdf/mdpdf';
 
-function main() {
+function main(): void {
     const args = process.argv.slice(2);
 
     if (args.join(' ') === '--help') {
@@ -16,25 +16,26 @@ function main() {
         process.exit(1);
     }
 
-    const [csvFilePath, pdfFilePath] = args;
+    const csvFilePath = args[0]!;
+    const pdfFilePath = args[1]!;
 
     const csv = fs.readFileSync(csvFilePath).toString();
     const report = generateReport(csv);
     markdownToPdf(report).then(buffer => fs.writeFileSync(pdfFilePath, buffer));
 }
 
-function checkInvalidInput(lines) {
-    const headers = lines[0];
+function checkInvalidInput(lines: string[][]): void {
+    const headers = lines[0]!;
 
     let foundProblem = false;
 
     for (let i = 1; i < lines.length; i++) {
-        if (lines[i].length > headers.length) {
+        if (lines[i]!.length > headers.length) {
             foundProblem = true;
             console.error(`Line #${i + 1} has more columns than there are candidates.`);
         }
 
-        const rankings = lines[i].filter(Boolean).map(x => +x).sort((a, b) => a - b);
+        const rankings = lines[i]!.filter(Boolean).map(x => +x).sort((a, b) => a - b);
         for (let j = 0; j < rankings.length; j++) {
             if (rankings[j] !== j + 1) {
                 foundProblem = true;
@@ -47,39 +48,57 @@ function checkInvalidInput(lines) {
     if (foundProblem) process.exit(1);
 }
 
-function generateReport(csv) {
+/**
+ * 0-based index of header row
+ */
+type Candidate = number;
+
+/**
+ * Array of Candidates, highest preference first.
+ */
+type Ballot = number[];
+
+type Distribution = ShareOfVotes[];
+
+type ShareOfVotes = {
+    candidate: Candidate;
+    count: number;
+    percent: number;
+};
+
+function generateReport(csv: string): string {
     let report = '';
 
     const lines = csv.split('\n').filter(Boolean).map(s => s.split(','));
     checkInvalidInput(lines);
 
-    const candidateNames = lines[0];
-    const initialVotes = lines.slice(1).map(arr => {
-        const rankings = [];
+    const candidateNames: string[] = lines[0]!;
+    const initialBallots: Ballot[] = lines.slice(1).map(arr => {
+        const ballot: Ballot = [];
         for (let candidate = 0; candidate <= arr.length; candidate++) {
             const rank = arr[candidate];
             if (!rank) continue;
-            rankings[+rank - 1] = candidate;
+            ballot[+rank - 1] = candidate;
         }
-        return rankings;
+        return ballot;
     });
 
     report += '# Instant Runoff Results\n\n';
 
-    function getDistribution(candidates, votes) {
-        const totals = candidates.map(candidate => {
-            const count = votes.filter(v => v[0] === candidate).length;
+    function getDistribution(candidates: Candidate[], ballots: Ballot[]): Distribution {
+        const shares: Distribution = candidates.map(candidate => {
+            const count = ballots.filter(v => v[0] === candidate).length;
 
             return {
                 candidate,
                 count,
-                percent: 100 * count / votes.length,
+                percent: 100 * count / ballots.length,
             };
         });
-        return totals.sort((a, b) => b.count - a.count);
+        return shares.sort((a, b) => b.count - a.count);
     }
 
-    function printTable(distribution) {
+    function printTable(distribution: Distribution): void {
         report += '| Candidate | Percent | Votes |\n';
         report += '|---------- | ------- | ------|\n';
         for (const { candidate, count, percent } of distribution) {
@@ -87,13 +106,13 @@ function generateReport(csv) {
         }
     }
 
-    const initialCandidates = Array.from({ length: candidateNames.length }, (_, i) => i);
-    const initialDistribution = getDistribution(initialCandidates, initialVotes);
+    const initialCandidates: Candidate[] = Array.from({ length: candidateNames.length }, (_, i) => i);
+    const initialDistribution: Distribution = getDistribution(initialCandidates, initialBallots);
 
     report += '## Initial results (phase 1)\n\n';
 
-    if (initialDistribution[0].percent > 50) {
-        report += `"${candidateNames[initialDistribution[0].candidate]}" won right away with ${initialDistribution[0].percent.toFixed(2)}% of the vote!\n\n`;
+    if (initialDistribution[0]!.percent > 50) {
+        report += `"${candidateNames[initialDistribution[0]!.candidate]}" won right away with ${initialDistribution[0]!.percent.toFixed(2)}% of the vote!\n\n`;
         printTable(initialDistribution);
         return report;
     }
@@ -101,31 +120,31 @@ function generateReport(csv) {
     report += 'No candidate surpassed the 50% threshold to win.\n\n';
     printTable(initialDistribution);
     report += '\n';
-    report += `${initialVotes.length} ballots were cast.\n\n`;
+    report += `${initialBallots.length} ballots were cast.\n\n`;
 
-    let candidates = JSON.parse(JSON.stringify(initialCandidates));
-    let votes = JSON.parse(JSON.stringify(initialVotes));
-    let distribution = initialDistribution;
+    let candidates: Candidate[] = [...initialCandidates];
+    let ballots: Ballot[] = initialBallots.map(ballot => [...ballot]);
+    let distribution: Distribution = initialDistribution;
 
-    function removeCandidates(toRemove) {
+    function removeCandidates(toRemove: Candidate[]) {
         candidates = candidates.filter(c => !toRemove.includes(c));
 
-        for (let i = 0; i < votes.length; i++) {
-            const vote = votes[i];
+        for (let i = 0; i < ballots.length; i++) {
+            const vote = ballots[i]!;
             for (let j = 0; j < vote.length; j++) {
-                if (toRemove.includes(vote[j])) {
+                if (toRemove.includes(vote[j]!)) {
                     vote.splice(j, 1);
                     j--;
                 }
             }
             if (vote.length === 0) {
-                votes.splice(i, 1);
+                ballots.splice(i, 1);
                 i--;
             }
         }
     }
 
-    const candidatesWithNoVotes = distribution
+    const candidatesWithNoVotes: Candidate[] = distribution
         .filter(({ percent }) => percent === 0)
         .map(({ candidate }) => candidate);
 
@@ -136,7 +155,7 @@ function generateReport(csv) {
         }
         report += '\n';
         removeCandidates(candidatesWithNoVotes);
-        distribution = getDistribution(candidates, votes);
+        distribution = getDistribution(candidates, ballots);
     }
 
     let phase = 1;
@@ -163,25 +182,25 @@ function generateReport(csv) {
 
         report += `## Phase ${phase}\n\n`;
 
-        distribution = getDistribution(candidates, votes);
+        distribution = getDistribution(candidates, ballots);
 
-        if (distribution[0].percent > 50) {
-            report += `"${candidateNames[distribution[0].candidate]}" wins with ${distribution[0].percent.toFixed(2)}% of the vote.\n\n`;
-            const eliminatedBallots = initialVotes.length - votes.length;
-            report += `${(100 * eliminatedBallots / initialVotes.length).toFixed(2)}% (${eliminatedBallots}/${initialVotes.length}) of ballots cast were eliminated.\n\n`;
+        if (distribution[0]!.percent > 50) {
+            report += `"${candidateNames[distribution[0]!.candidate]}" wins with ${distribution[0]!.percent.toFixed(2)}% of the vote.\n\n`;
+            const eliminatedBallots = initialBallots.length - ballots.length;
+            report += `${(100 * eliminatedBallots / initialBallots.length).toFixed(2)}% (${eliminatedBallots}/${initialBallots.length}) of ballots cast were eliminated.\n\n`;
             printTable(distribution);
             return report;
         }
 
         report += 'No candidate surpassed the 50% threshold to win.\n\n';
-        const eliminatedBallots = initialVotes.length - votes.length;
-        report += `${(100 * eliminatedBallots / initialVotes.length).toFixed(2)}% (${eliminatedBallots}/${initialVotes.length}) of ballots cast have been eliminated so far.\n\n`;
+        const eliminatedBallots = initialBallots.length - ballots.length;
+        report += `${(100 * eliminatedBallots / initialBallots.length).toFixed(2)}% (${eliminatedBallots}/${initialBallots.length}) of ballots cast have been eliminated so far.\n\n`;
         printTable(distribution);
         report += '\n';
     }
 }
 
-function help(log) {
+function help(log: (s: string) => void): void {
     log('Usage: ./main.js <input CSV> <output PDF>');
     log('');
     log('The input CSV file should have one header row followed by all the ballot rows.');
